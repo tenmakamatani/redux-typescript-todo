f9bd2ecf858d32a5ff5c4cc9c373df6f63bfa2ba
tenmakamatani <tenma_18n1102540@nnn.ed.jp>
2019-09-25 18:37:18 +0900

    [update]redux導入

---
+++ src/client/components/App.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+const component: React.SFC = () => {
+    return (
+        <div />
+    );
+}
+
+export default component;
--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,36 +1,16 @@
-"use strict";
-
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import Button from "./component/Button";
+import { createStore } from "redux";
 
-interface IProps {
+import App from "./components/App";
+import { rootReducer } from "./modules/index";
+import { Provider } from "react-redux";
 
-};
+const store = createStore(rootReducer);
 
-interface IState {
-    count: number;
-};
-
-class App extends React.Component<IProps, IState> {
-    constructor(props: IProps) {
-        super(props);
-        this.state = {
-            count: 0
-        };
-        this.countUp = this.countUp.bind(this);
-    }
-    countUp(): void {
-        this.setState({ count: this.state.count + 1 });
-    }
-    render() {
-        return (
-            <div>
-                <Button onClick={ this.countUp }></Button>
-                <h1>{ this.state.count }</h1>
-            </div>
-        )
-    }
-}
-
-ReactDOM.render(<App />, document.getElementById("index"));
+ReactDOM.render(
+    <Provider store={ store }>
+        <App />
+    </Provider>,
+    document.getElementById("root")
+);
