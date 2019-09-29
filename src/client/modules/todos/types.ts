95732611a5e8edafc44a7e0e1a32c3d8c3d1c707
tenmakamatani <tenma_18n1102540@nnn.ed.jp>
2019-09-29 22:44:19 +0900

    add type of delete todo

--- src/client/modules/todos/types.ts
+++ src/client/modules/todos/types.ts
@@ -3,11 +3,22 @@ export type AddTodoPayload = {
     text: string;
 };
 
+export type DeleteTodoPayload = {
+    id: number;
+}
+
+
 // action types
 export type AddTodoAction = {
     type: "ADD_TODO";
     payload: AddTodoPayload;
 };
+
+export type DeleteTodoAction = {
+    type: "DELETE_TODO";
+    payload: DeleteTodoPayload;
+}
+
 
 // state types
 export interface ITodo {
