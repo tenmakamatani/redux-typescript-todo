24b3fde3813cb974f52b8bce6df9fbc328fde3a5
tenmakamatani <tenma_18n1102540@nnn.ed.jp>
2019-10-05 23:36:42 +0900

    change component name and add UpdateTodo types

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -3,14 +3,14 @@ import * as ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import Root from "./pages/Top/components/Top";
+import Top from "./pages/Top";
 import { store } from "./modules/store";
 
 ReactDOM.render(
     <Provider store={ store }>
         <Router>
-            <Route exact path="/" component={ Root } />
+            <Route exact path="/" component={ Top } />
         </Router>
     </Provider>,
     document.getElementById("root")
-);
+)
